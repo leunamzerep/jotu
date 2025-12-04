f371118a8ac5281a80e1fcd0c85a2795aa833b48
Manuel Pérez <manuelsph.315@gmail.com>
2025-12-04 00:21:00 -0500

    adapt uper camel case on app component

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./i18n/config";
 import { StrictMode } from 'react'
 import { HelmetProvider } from "react-helmet-async";
 import { createRoot } from 'react-dom/client'
-import { App } from './app'
+import { App } from "./App";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
