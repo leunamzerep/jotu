847bec850bb50abd083284100d7fde11a7b5a46d
Manuel Pérez <manuelsph.315@gmail.com>
2025-12-04 14:41:36 -0500

    first aproach to homePage

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -9,6 +9,7 @@ export const Footer = () => {
 
   const { t } = useTranslation();
   const whaLink: string = 'https://wa.me/19179156583?text=%F0%9F%91%8B%20%C2%A1Hola!%0AGracias%20por%20comunicarte%20con%20JoTu%20Solutions%20LLC.%0A%0A%F0%9F%95%92%20Nuestro%20horario%20de%20atenci%C3%B3n%20es:%0ALunes%20a%20Viernes%20de%209:00%20a.m.%20a%205:00%20p.m.%20(hora%20Miami).%0A%0A%E2%9C%85%20En%20breve,%20uno%20de%20nuestros%20representantes%20te%20responder%C3%A1.%0AMientras%20tanto,%20por%20favor%20dinos%20tu%20nombre%20y%20c%C3%B3mo%20podemos%20ayudarte.%0A%0A%F0%9F%93%8C%20%C2%A1Gracias%20por%20elegir%20a%20JoTu%20Solutions%20LLC'
+  const mailLink: string = t('footer.mailRedirect')
   const footerRef = useRef<HTMLDivElement>(null);
   const [buttonBottom, setButtonBottom] = useState(30);
 
@@ -58,8 +59,8 @@ export const Footer = () => {
             <a className={styles.media} href="https://www.facebook.com/people/JOTU-Solutions-LLC/61576996550658" target="_blank">
               <i className="fa-brands fa-facebook"></i>
             </a>
-            <a className={styles.media} href={whaLink} target="_blank">
-              <i className="fa-brands fa-whatsapp"></i>
+            <a className={styles.media} href={mailLink} target="_blank">
+              <i className="fa-regular fa-envelope"></i>
             </a>
           </div>
           <p className={styles.legalAdvice}>© 2024. All rights reserved.</p>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import { Helmet } from "react-helmet-async";
+import styles from './Home.module.css'
 
 export const Home = () => {
 
@@ -13,25 +14,27 @@ export const Home = () => {
         <link rel="canonical" href="https://jotusolutions.net" />
       </Helmet>
 
-      <main>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
-        <h1>{t('header.home')}</h1>
+      <main className={styles.mainContainer}>
+        <div className={styles.titleContainer}>
+          <img src="src/assets/img/mainImg.jpg" className={styles.mainImg} />
+          <h1 className={styles.mainTitle}>{t('home.main')}</h1>
+        </div>
+        <div className={styles.infoContainer}>
+          <div className={styles.item}>
+            <img src="src/assets/img/home1.webp" className={styles.home1} />
+            <div className={styles.textContainer}>
+              <h2 className={styles.itemTitle}>{t('home.title1')}</h2>
+              <h3 className={styles.itemText}>{t('home.text1')}</h3>
+            </div>
+          </div>
+          <div className={styles.item}>
+            <img src="src/assets/img/home2.jpg" className={styles.home2} />
+            <div className={styles.textContainer}>
+              <h2 className={styles.itemTitle}>{t('home.title2')}</h2>
+              <h3 className={styles.itemText}>{t('home.text2')}</h3>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
