f87fcaf46d6f6a7792a8ee9adbdab03c6a654381
Manuel Pérez <manuelsph.315@gmail.com>
2025-12-09 22:44:11 -0500

    about us init

--- src/pages/aboutUs/AboutUs.tsx
+++ src/pages/aboutUs/AboutUs.tsx
@@ -1,10 +1,14 @@
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 
 import { SeoEntry } from "../../components/seoEntry/SeoEntry";
 import { ContactCard } from "../../components/contactCard/ContactCard";
 
+
 import aboutImg from "../../assets/img/aboutImg.jpg"
+import about1 from "../../assets/img/about1.jpg";
+import about2 from "../../assets/img/about2.jpg";
 import styles from './AboutUs.module.css'
 
 type AboutProps = {
@@ -14,7 +18,15 @@ type AboutProps = {
 export const AboutUs = ({ canAnimate }: AboutProps) => {
 
   const { t } = useTranslation();
-  const h1 = t("about.main")
+  const h1 = t("about.h1")
+
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (!canAnimate) return;
+    const id = requestAnimationFrame(() => setAnimate(true));
+    return () => cancelAnimationFrame(id);
+  }, [canAnimate]);
 
   return (
     <>
@@ -26,7 +38,28 @@ export const AboutUs = ({ canAnimate }: AboutProps) => {
 
       <main>
         <SeoEntry imgPath={aboutImg} content={h1} canAnimate={canAnimate} />
-        <h1 className={styles.p}>{t('header.aboutUs')}</h1>
+        <div>
+          <div>
+            <h2></h2>
+            <h3></h3>
+          </div>
+          <div className={styles.infoContainer}>
+            <div className={`${styles.item} ${animate ? styles.enter : ""}`} >
+              <div className={styles.textContainer}>
+                <h2 className={styles.itemTitle}>{t("about.h22")}</h2>
+                <h3 className={styles.itemText}>{t("about.h32")}</h3>
+              </div>
+              <img src={about1} className={styles.about1} />
+            </div>
+            <div className={`${styles.item} ${animate ? styles.enter : ""}`} >
+              <div className={styles.textContainer}>
+                <h2 className={styles.itemTitle}>{t("about.h23")}</h2>
+                <h3 className={styles.itemText}>{t("about.h33")}</h3>
+              </div>
+              <img src={about2} className={styles.about2} />
+            </div>
+          </div>
+        </div>
         <ContactCard />
       </main>
     </>
