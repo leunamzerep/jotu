3d84f84cf6aeaaca255814af54f7f861e35ccbae
Manuel Pérez <manuelsph.315@gmail.com>
2025-12-21 23:58:30 -0500

    return to initial vite congif

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import vitePrerender from "vite-plugin-prerender";
-import path from "node:path";
-import { prerenderRoutes } from "./src/prerender-routes";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/ 
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
-    vitePrerender({
-      staticDir: path.join(__dirname, "dist"),
-      routes: prerenderRoutes,
-    }),
   ],
   build: {
     manifest: true,
